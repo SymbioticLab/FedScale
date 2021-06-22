9ad03881ed0e9712f5a0695005a5e3cb43bbc64c
Zhifeng Jiang <samuelgong2017@gmail.com>
2021-06-22 16:22:57 +0800

    Update shutdown.py

--- core/evals/shutdown.py
+++ core/evals/shutdown.py
@@ -5,8 +5,8 @@ import argparse
 job_name = sys.argv[1]
 
 if job_name == 'all':
-    os.system("ps -ef | grep python | grep Kuiper > kuiper_running_temp")
+    os.system("ps -ef | grep python | grep FedScale > fedscale_running_temp")
 else:
-    os.system("ps -ef | grep python | grep job_name={} > kuiper_running_temp".format(job_name))
-[os.system("kill -9 "+str(l.split()[1])) for l in open("kuiper_running_temp").readlines()]
-os.system("rm kuiper_running_temp")
+    os.system("ps -ef | grep python | grep job_name={} > fedscale_running_temp".format(job_name))
+[os.system("kill -9 "+str(l.split()[1]) + " 1>/dev/null 2>&1") for l in open("fedscale_running_temp").readlines()]
+os.system("rm fedscale_running_temp")
