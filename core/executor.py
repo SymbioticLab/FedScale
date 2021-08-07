add711f0f1983d495a8f9871d24442c4dbda7388
Zhifeng Jiang <samuelgong2017@gmail.com>
2021-08-07 14:35:49 +0800

    Update executor.py

--- core/executor.py
+++ core/executor.py
@@ -38,16 +38,19 @@ class Executor(object):
         self.setup_seed(seed=self.this_rank)
 
         # set up device
-        if self.args.use_cuda and self.device == None:
-            for i in range(torch.cuda.device_count()):
-                try:
-                    self.device = torch.device('cuda:'+str(i))
-                    torch.cuda.set_device(i)
-                    print(torch.rand(1).to(device=self.device))
-                    logging.info(f'End up with cuda device ({self.device})')
-                    break
-                except Exception as e:
-                    assert i != torch.cuda.device_count()-1, 'Can not find available GPUs'
+        if self.args.use_cuda:
+            if self.device == None:
+                for i in range(torch.cuda.device_count()):
+                    try:
+                        self.device = torch.device('cuda:'+str(i))
+                        torch.cuda.set_device(i)
+                        print(torch.rand(1).to(device=self.device))
+                        logging.info(f'End up with cuda device ({self.device})')
+                        break
+                    except Exception as e:
+                        assert i != torch.cuda.device_count()-1, 'Can not find available GPUs'
+            else:
+                torch.cuda.set_device(self.device)
 
         self.init_control_communication(self.args.ps_ip, self.args.manager_port)
         self.init_data_communication()
