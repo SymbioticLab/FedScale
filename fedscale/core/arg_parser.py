7178d93d05412697bd83424e91108de485bdd978
Yinwei_Dai <dywsjtuji@gmail.com>
2022-04-02 01:41:29 -0400

    tweak

--- fedscale/core/arg_parser.py
+++ fedscale/core/arg_parser.py
@@ -26,7 +26,7 @@ parser.add_argument('--exploration_decay', type=float, default=0.98)
 parser.add_argument('--sample_window', type=float, default=5.0)
 parser.add_argument('--device_avail_file', type=str, default=None)
 parser.add_argument('--clock_factor', type=float, default=1.0, help="Refactor the clock time given the profile")
-
+parser.add_argument('--max_message_length', type=int, default=50000000)
 
 # The configuration of model and dataset
 parser.add_argument('--data_dir', type=str, default='~/cifar10/')
