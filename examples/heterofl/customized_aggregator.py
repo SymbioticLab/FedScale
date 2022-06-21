84a237eef3e379f24c349cda61a6ed6adc2aa4ef
zyxum <yuxuanzh@umich.edu>
2022-06-21 19:04:56 -0400

    fix legacy codes

--- examples/heterofl/customized_aggregator.py
+++ examples/heterofl/customized_aggregator.py
@@ -26,7 +26,7 @@ class Customized_Aggregator(Aggregator):
             client_cfg = self.client_conf.get(client_to_run, self.args)
 
             exe_cost = self.client_manager.getCompletionTime(client_to_run,
-                                    batch_size=client_cfg.batch_size, upload_epoch=client_cfg.local_steps,
+                                    batch_size=client_cfg.batch_size, upload_step=client_cfg.local_steps,
                                     upload_size=self.model_update_size, download_size=self.model_update_size)
 
             roundDuration = exe_cost['computation'] + exe_cost['communication']
