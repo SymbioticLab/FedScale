ee0a479b33302934c02d8f28b93015e2ac810735
continue-revolution <3357667574@qq.com>
2022-12-05 06:35:25 +0800

    remove demo

--- fedscale/core/execution/android/install.sh
+++ fedscale/core/execution/android/install.sh
@@ -5,9 +5,9 @@ rm -rf MNN/resource/model/*
 cp -r assets/* MNN/resource/model/
 rm -rf MNN/source/jni
 cp -r jni MNN/source
-cp demo/build.gradle MNN/project/android/demo
-cp demo/app/build.gradle MNN/project/android/demo/app
+cp build.gradle MNN/project/android/demo
+cp app/build.gradle MNN/project/android/demo/app
 rm -rf MNN/project/android/demo/gradle/
-cp -r demo/gradle MNN/project/android/demo/
+cp -r gradle MNN/project/android/demo/
 rm -rf MNN/project/android/demo/app/src
-cp -r demo/app/src MNN/project/android/demo/app/
+cp -r app/src MNN/project/android/demo/app/
