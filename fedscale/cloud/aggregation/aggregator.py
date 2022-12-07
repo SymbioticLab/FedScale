c044e7dc42bc253a39c3379d367ba106a1a250aa
Yile Gu <yilegu@umich.edu>
2022-12-06 18:18:31 -0600

    Start to replace tensorboard with wandb

--- fedscale/cloud/aggregation/aggregator.py
+++ fedscale/cloud/aggregation/aggregator.py
@@ -6,6 +6,7 @@ from concurrent import futures
 
 import grpc
 import torch
+import wandb
 from torch.utils.tensorboard import SummaryWriter
 
 import fedscale.cloud.channels.job_api_pb2_grpc as job_api_pb2_grpc
@@ -98,6 +99,8 @@ class Aggregator(job_api_pb2_grpc.JobServiceServicer):
                                 'gradient_policy': args.gradient_policy, 'task': args.task, 'perf': collections.OrderedDict()}
 
         self.log_writer = SummaryWriter(log_dir=logger.logDir)
+        self.wandb = wandb
+        self.wandb.init(project=f'fedscale-{args.job_name}')
 
         # ======== Task specific ============
         self.init_task_context()
@@ -583,7 +586,17 @@ class Aggregator(job_api_pb2_grpc.JobServiceServicer):
             'FAR/round_duration (min)', self.round_duration/60., self.round)
         self.log_writer.add_histogram(
             'FAR/client_duration (min)', self.flatten_client_duration, self.round)
-
+        print(f"round_duration: {self.round_duration/60.} at round {self.round}")
+        self.wandb.log({
+            'Train/round_to_loss': avg_loss,
+            'FAR/round_duration (min)': self.round_duration/60.,
+            'FAR/client_duration (min)': self.flatten_client_duration,
+            'FAR/time_to_round (min)': self.global_virtual_clock/60.,
+        }, step=self.round)
+        # self.wandb.log({
+        #     'FAR/time_to_train_loss (min)': avg_loss,
+        # }, step=int(self.global_virtual_clock/60.)) # wandb only accepts integer step, so we lose some accuracy in global time here
+        
     def log_test_result(self):
         """Log testing result on TensorBoard
         """
@@ -595,6 +608,15 @@ class Aggregator(job_api_pb2_grpc.JobServiceServicer):
                                    self.global_virtual_clock/60.)
         self.log_writer.add_scalar('FAR/time_to_test_accuracy (min)', self.testing_history['perf'][self.round]['top_1'],
                                    self.global_virtual_clock/60.)
+        self.wandb.log({
+            'Test/round_to_loss': self.testing_history['perf'][self.round]['loss'],
+            'Test/round_to_accuracy': self.testing_history['perf'][self.round]['top_1'],
+            'FAR/round_duration (min)': self.round_duration/60.,
+        }, step=self.round)
+        # self.wandb.log({
+        #     'FAR/time_to_test_loss (min)': self.testing_history['perf'][self.round]['loss'],
+        #     'FAR/time_to_test_accuracy (min)': self.testing_history['perf'][self.round]['top_1'],
+        # }, step=int(self.global_virtual_clock/60.)) # wandb only accepts integer step, so we lose some accuracy in global time here
 
     def deserialize_response(self, responses):
         """Deserialize the response from executor
