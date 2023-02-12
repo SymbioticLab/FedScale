7a1992542553d1cc7893a7509932188de8762987
continue-revolution <3357667574@qq.com>
2023-02-12 11:55:50 +0800

    update convert.sh

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -2,5 +2,5 @@
 cd ../../cloud/aggregation/cache
 
 # convert model: onnx->mnn->json.
-../../../scripts/MNNConvert -f ONNX --modelFile model.onnx --MNNModel model.mnn --forTraining --bizCode fedscale
-../../../scripts/MNNDump2Json model.mnn model.json 
+../../../../scripts/MNNConvert -f ONNX --modelFile model.onnx --MNNModel model.mnn --forTraining --bizCode fedscale
+../../../../scripts/MNNDump2Json model.mnn model.json 
