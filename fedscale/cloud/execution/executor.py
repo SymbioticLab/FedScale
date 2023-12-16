c06b14db19d854e9881baf985447a23eee79d16d
EricDinging <ericdingg16@outlook.com>
2023-12-16 11:55:52 -0500

    Turn off optimizer in executor model update

--- fedscale/cloud/execution/executor.py
+++ fedscale/cloud/execution/executor.py
@@ -33,7 +33,9 @@ class Executor(object):
         # initiate the executor log path, and executor ips
         logger.initiate_client_setting()
 
-        self.model_adapter = self.get_client_trainer(args).get_model_adapter(init_model())
+        self.model_adapter = self.get_client_trainer(args).get_model_adapter(
+            init_model()
+        )
 
         self.args = args
         self.num_executors = args.num_executors
@@ -45,8 +47,7 @@ class Executor(object):
         self.training_sets = self.test_dataset = None
 
         # ======== channels ========
-        self.aggregator_communicator = ClientConnections(
-            args.ps_ip, args.ps_port)
+        self.aggregator_communicator = ClientConnections(args.ps_ip, args.ps_port)
 
         # ======== runtime information ========
         self.collate_fn = None
@@ -56,28 +57,28 @@ class Executor(object):
         self.event_queue = collections.deque()
 
         if args.wandb_token != "":
-            os.environ['WANDB_API_KEY'] = args.wandb_token
+            os.environ["WANDB_API_KEY"] = args.wandb_token
             self.wandb = wandb
             if self.wandb.run is None:
-                self.wandb.init(project=f'fedscale-{args.job_name}',
-                                name=f'executor{args.this_rank}-{args.time_stamp}',
-                                group=f'{args.time_stamp}')
+                self.wandb.init(
+                    project=f"fedscale-{args.job_name}",
+                    name=f"executor{args.this_rank}-{args.time_stamp}",
+                    group=f"{args.time_stamp}",
+                )
             else:
                 logging.error("Warning: wandb has already been initialized")
-            
+
         else:
             self.wandb = None
         super(Executor, self).__init__()
 
     def setup_env(self):
-        """Set up experiments environment
-        """
+        """Set up experiments environment"""
         logging.info(f"(EXECUTOR:{self.this_rank}) is setting up environ ...")
         self.setup_seed(seed=1)
 
     def setup_communication(self):
-        """Set up grpc connection
-        """
+        """Set up grpc connection"""
         self.init_control_communication()
         self.init_data_communication()
 
@@ -101,8 +102,7 @@ class Executor(object):
         self.aggregator_communicator.connect_to_server()
 
     def init_data_communication(self):
-        """In charge of jumbo data traffics (e.g., fetch training result)
-        """
+        """In charge of jumbo data traffics (e.g., fetch training result)"""
         pass
 
     def init_data(self):
@@ -115,20 +115,27 @@ class Executor(object):
         train_dataset, test_dataset = init_dataset()
         if self.args.task == "rl":
             return train_dataset, test_dataset
-        if self.args.task == 'nlp':
+        if self.args.task == "nlp":
             self.collate_fn = collate
-        elif self.args.task == 'voice':
+        elif self.args.task == "voice":
             self.collate_fn = voice_collate_fn
         # load data partitionxr (entire_train_data)
         logging.info("Data partitioner starts ...")
 
         training_sets = DataPartitioner(
-            data=train_dataset, args=self.args, numOfClass=self.args.num_class)
+            data=train_dataset, args=self.args, numOfClass=self.args.num_class
+        )
         training_sets.partition_data_helper(
-            num_clients=self.args.num_participants, data_map_file=self.args.data_map_file)
+            num_clients=self.args.num_participants,
+            data_map_file=self.args.data_map_file,
+        )
 
         testing_sets = DataPartitioner(
-            data=test_dataset, args=self.args, numOfClass=self.args.num_class, isTest=True)
+            data=test_dataset,
+            args=self.args,
+            numOfClass=self.args.num_class,
+            isTest=True,
+        )
         testing_sets.partition_data_helper(num_clients=self.num_executors)
 
         logging.info("Data partitioner completes ...")
@@ -136,8 +143,7 @@ class Executor(object):
         return training_sets, testing_sets
 
     def run(self):
-        """Start running the executor by setting up execution and communication environment, and monitoring the grpc message.
-        """
+        """Start running the executor by setting up execution and communication environment, and monitoring the grpc message."""
         self.setup_env()
         self.training_sets, self.testing_sets = self.init_data()
         self.setup_communication()
@@ -184,7 +190,7 @@ class Executor(object):
 
         """
         self.round += 1
-        self.model_adapter.set_weights(model_weights)
+        self.model_adapter.set_weights(model_weights, is_aggregator=False)
 
     def Train(self, config):
         """Load train config and data to start training on that client
@@ -196,20 +202,25 @@ class Executor(object):
             tuple (int, dictionary): The client id and train result
 
         """
-        client_id, train_config = config['client_id'], config['task_config']
+        client_id, train_config = config["client_id"], config["task_config"]
 
-        if 'model' not in config or not config['model']:
+        if "model" not in config or not config["model"]:
             raise "The 'model' object must be a non-null value in the training config."
         client_conf = self.override_conf(train_config)
         train_res = self.training_handler(
-            client_id=client_id, conf=client_conf, model=config['model'])
+            client_id=client_id, conf=client_conf, model=config["model"]
+        )
 
         # Report execution completion meta information
         response = self.aggregator_communicator.stub.CLIENT_EXECUTE_COMPLETION(
             job_api_pb2.CompleteRequest(
-                client_id=str(client_id), executor_id=self.executor_id,
-                event=commons.CLIENT_TRAIN, status=True, msg=None,
-                meta_result=None, data_result=None
+                client_id=str(client_id),
+                executor_id=self.executor_id,
+                event=commons.CLIENT_TRAIN,
+                status=True,
+                msg=None,
+                meta_result=None,
+                data_result=None,
             )
         )
         self.dispatch_worker_events(response)
@@ -224,21 +235,24 @@ class Executor(object):
 
         """
         test_res = self.testing_handler()
-        test_res = {'executorId': self.this_rank, 'results': test_res}
+        test_res = {"executorId": self.this_rank, "results": test_res}
 
         # Report execution completion information
         response = self.aggregator_communicator.stub.CLIENT_EXECUTE_COMPLETION(
             job_api_pb2.CompleteRequest(
-                client_id=self.executor_id, executor_id=self.executor_id,
-                event=commons.MODEL_TEST, status=True, msg=None,
-                meta_result=None, data_result=self.serialize_response(test_res)
+                client_id=self.executor_id,
+                executor_id=self.executor_id,
+                event=commons.MODEL_TEST,
+                status=True,
+                msg=None,
+                meta_result=None,
+                data_result=self.serialize_response(test_res),
             )
         )
         self.dispatch_worker_events(response)
 
     def Stop(self):
-        """Stop the current executor
-        """
+        """Stop the current executor"""
         logging.info(f"Terminating the executor ...")
         self.aggregator_communicator.close_sever_connection()
         self.received_stop_request = True
@@ -255,7 +269,7 @@ class Executor(object):
         return self.training_sets.getSize()
 
     def override_conf(self, config):
-        """ Override the variable arguments for different client
+        """Override the variable arguments for different client
 
         Args:
             config (dictionary): The client runtime config.
@@ -280,7 +294,7 @@ class Executor(object):
         if conf.engine == commons.TENSORFLOW:
             return TensorflowClient(conf)
         elif conf.engine == commons.PYTORCH:
-            if conf.task == 'rl':
+            if conf.task == "rl":
                 return RLClient(conf)
             else:
                 return TorchClient(conf)
@@ -300,14 +314,21 @@ class Executor(object):
         self.model_adapter.set_weights(model)
         conf.client_id = client_id
         conf.tokenizer = tokenizer
-        client_data = self.training_sets if self.args.task == "rl" else \
-            select_dataset(client_id, self.training_sets,
-                           batch_size=conf.batch_size, args=self.args,
-                           collate_fn=self.collate_fn
-                           )
+        client_data = (
+            self.training_sets
+            if self.args.task == "rl"
+            else select_dataset(
+                client_id,
+                self.training_sets,
+                batch_size=conf.batch_size,
+                args=self.args,
+                collate_fn=self.collate_fn,
+            )
+        )
         client = self.get_client_trainer(self.args)
         train_res = client.train(
-            client_data=client_data, model=self.model_adapter.get_model(), conf=conf)
+            client_data=client_data, model=self.model_adapter.get_model(), conf=conf
+        )
 
         return train_res
 
@@ -321,25 +342,33 @@ class Executor(object):
             dictionary: The test result
 
         """
-        test_config = self.override_conf({
-            'rank': self.this_rank,
-            'memory_capacity': self.args.memory_capacity,
-            'tokenizer': tokenizer
-        })
+        test_config = self.override_conf(
+            {
+                "rank": self.this_rank,
+                "memory_capacity": self.args.memory_capacity,
+                "tokenizer": tokenizer,
+            }
+        )
         client = self.get_client_trainer(test_config)
-        data_loader = select_dataset(self.this_rank, self.testing_sets,
-                                     batch_size=self.args.test_bsz, args=self.args,
-                                     isTest=True, collate_fn=self.collate_fn)
+        data_loader = select_dataset(
+            self.this_rank,
+            self.testing_sets,
+            batch_size=self.args.test_bsz,
+            args=self.args,
+            isTest=True,
+            collate_fn=self.collate_fn,
+        )
 
-        test_results = client.test(data_loader, self.model_adapter.get_model(), test_config)
+        test_results = client.test(
+            data_loader, self.model_adapter.get_model(), test_config
+        )
         self.log_test_result(test_results)
         gc.collect()
 
         return test_results
 
     def client_register(self):
-        """Register the executor information to the aggregator
-        """
+        """Register the executor information to the aggregator"""
         start_time = time.time()
         while time.time() - start_time < 180:
             try:
@@ -348,27 +377,29 @@ class Executor(object):
                         client_id=self.executor_id,
                         executor_id=self.executor_id,
                         executor_info=self.serialize_response(
-                            self.report_executor_info_handler())
+                            self.report_executor_info_handler()
+                        ),
                     )
                 )
                 self.dispatch_worker_events(response)
                 break
             except Exception as e:
-                logging.warning(f"Failed to connect to aggregator {e}. Will retry in 5 sec.")
+                logging.warning(
+                    f"Failed to connect to aggregator {e}. Will retry in 5 sec."
+                )
                 time.sleep(5)
 
     def client_ping(self):
-        """Ping the aggregator for new task
-        """
-        response = self.aggregator_communicator.stub.CLIENT_PING(job_api_pb2.PingRequest(
-            client_id=self.executor_id,
-            executor_id=self.executor_id
-        ))
+        """Ping the aggregator for new task"""
+        response = self.aggregator_communicator.stub.CLIENT_PING(
+            job_api_pb2.PingRequest(
+                client_id=self.executor_id, executor_id=self.executor_id
+            )
+        )
         self.dispatch_worker_events(response)
 
     def event_monitor(self):
-        """Activate event handler once receiving new message
-        """
+        """Activate event handler once receiving new message"""
         logging.info("Start monitoring events ...")
         self.client_register()
 
@@ -380,24 +411,34 @@ class Executor(object):
                 if current_event == commons.CLIENT_TRAIN:
                     train_config = self.deserialize_response(request.meta)
                     train_model = self.deserialize_response(request.data)
-                    train_config['model'] = train_model
-                    train_config['client_id'] = int(train_config['client_id'])
+                    train_config["model"] = train_model
+                    train_config["client_id"] = int(train_config["client_id"])
                     client_id, train_res = self.Train(train_config)
 
                     # Upload model updates
                     future_call = self.aggregator_communicator.stub.CLIENT_EXECUTE_COMPLETION.future(
-                        job_api_pb2.CompleteRequest(client_id=str(client_id), executor_id=self.executor_id,
-                                                    event=commons.UPLOAD_MODEL, status=True, msg=None,
-                                                    meta_result=None, data_result=self.serialize_response(train_res)
-                                                    ))
-                    future_call.add_done_callback(lambda _response: self.dispatch_worker_events(_response.result()))
+                        job_api_pb2.CompleteRequest(
+                            client_id=str(client_id),
+                            executor_id=self.executor_id,
+                            event=commons.UPLOAD_MODEL,
+                            status=True,
+                            msg=None,
+                            meta_result=None,
+                            data_result=self.serialize_response(train_res),
+                        )
+                    )
+                    future_call.add_done_callback(
+                        lambda _response: self.dispatch_worker_events(
+                            _response.result()
+                        )
+                    )
 
                 elif current_event == commons.MODEL_TEST:
                     self.Test(self.deserialize_response(request.meta))
 
                 elif current_event == commons.UPDATE_MODEL:
                     model_weights = self.deserialize_response(request.data)
-                    self.UpdateModel(model_weights, is_aggregator=False)
+                    self.UpdateModel(model_weights)
 
                 elif current_event == commons.SHUT_DOWN:
                     self.Stop()
@@ -409,22 +450,26 @@ class Executor(object):
                 try:
                     self.client_ping()
                 except Exception as e:
-                    logging.info(f"Caught exception {e} from aggregator, terminating executor {self.this_rank} ...")
+                    logging.info(
+                        f"Caught exception {e} from aggregator, terminating executor {self.this_rank} ..."
+                    )
                     self.Stop()
 
-    
     def log_test_result(self, test_res):
-        """Log test results to wandb server if enabled
-        """
+        """Log test results to wandb server if enabled"""
         acc = round(test_res["top_1"] / test_res["test_len"], 4)
         acc_5 = round(test_res["top_5"] / test_res["test_len"], 4)
         test_loss = test_res["test_loss"] / test_res["test_len"]
         if self.wandb != None:
-            self.wandb.log({
-                'Test/round_to_top1_accuracy': acc,
-                'Test/round_to_top5_accuracy': acc_5,
-                'Test/round_to_loss': test_loss,
-            }, step=self.round)
+            self.wandb.log(
+                {
+                    "Test/round_to_top1_accuracy": acc,
+                    "Test/round_to_top5_accuracy": acc_5,
+                    "Test/round_to_loss": test_loss,
+                },
+                step=self.round,
+            )
+
 
 if __name__ == "__main__":
     executor = Executor(parser.args)
